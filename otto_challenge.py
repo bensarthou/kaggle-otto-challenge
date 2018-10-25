a51a61d8e1e94a169484328a0a72fd691621c0f6
bensarthou <bsarthou@hotmail.fr>
2018-10-25 21:45:51 +0200

    [+] adding tsne/pca manifold, and feature importance visualisation

--- otto_challenge.py
+++ otto_challenge.py
@@ -51,11 +51,13 @@ if __name__ == "__main__":
     # X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
 
     print('Training TSNE...')
-    X_embedded = TSNE(n_components=2, verbose=10).fit_transform(X[:10000])
+    X_embedded = TSNE(n_components=2, verbose=10).fit_transform(X[:10])
     print('End training')
     plt.figure()
-    for class_i in np.unique(y):
-        plt.scatter(X_embedded[y[:10000] == class_i, 0], X_embedded[y[:10000] == class_i, 1], label=str(y))
+    l_colors = ['r', 'g', 'b', 'y', 'k', 'c', 'm']
+    for idx, class_i in enumerate(np.unique(y)):
+        plt.scatter(X_embedded[y[:10000] == class_i, 0], X_embedded[y[:10000] == class_i, 1],
+        label=str(y), color=l_colors[i])
     plt.legend()
     plt.show()
     # models = []
@@ -102,59 +104,59 @@ if __name__ == "__main__":
     #               'alpha':[0.001]}
     # models.append(("MLPClassifier", MLPClassifier(), parameters))
     #
-    # ### ALL PARAMS
-    # # # Logistic Regression
-    # # parameters = {'penalty':['l1', 'l2'], 'C':[0.1, 1., 10.]}
-    # # models.append(("LogisticRegression", LogisticRegression(), parameters))
-    # #
-    # # # SVM
-    # # parameters = {'kernel':['linear', 'rbf', 'poly', 'sigmoid'], 'C':[0.1, 1., 10., 100], 'probability':True}
-    # # models.append(("SVC", SVC(), parameters))
-    # #
-    # # # KNeighbors
-    # # parameters = {'n_neighbors':[3, 5, 7], 'leaf_size':[10, 20, 30, 40], 'p':[1, 2]}
-    # # models.append(("KNeighbors", KNeighborsClassifier(), parameters))
-    # #
-    # # # DecisionTree
-    # # parameters = {'splitter':['best', 'random'], 'criterion':['gini', 'entropy'],
-    # #               'max_depth':[3, 5, 10, 20], 'min_samples_split':[2, 3, 5]}
-    # # models.append(("DecisionTree", DecisionTreeClassifier(), parameters))
-    # #
-    # # # RandomForestClassifier
-    # # parameters = {'n_estimators':[10, 50, 100], 'criterion':['gini', 'entropy'],
-    # #               'max_depth':[None, 3, 10], 'min_samples_split':[2, 3, 5]}
-    # # models.append(("RandomForest", RandomForestClassifier(), parameters))
-    # #
-    # # # GradientBoostingClassifier
-    # # parameters = {'loss':['deviance', 'exponential'], 'n_estimators':[10, 50, 100],
-    # #               'criterion':['friedman_mse', 'mae'], 'max_depth':[3, 10],
-    # #               'min_samples_split':[2, 3, 5]}
-    # # models.append(("GradientBoostingClassifier", GradientBoostingClassifier(), parameters))
-    # #
-    # # # Multi-Layer Classifier
-    # # parameters = {'hidden_layer_sizes':[(70,), (40, 40), (20, 20, 20)],
-    # #               'activation':['logistic', 'relu'],
-    # #               'batch_size':[128, 256],
-    # #               'alpha':[0.001, 0.0001]}
-    # # models.append(("MLPClassifier", MLPClassifier(), parameters))
-    #
-    #
-    # results = []
-    # names = []
-    # nb_cross_val = 2
-    # for name, model, parameter in models:
-    #     print(name)
-    #     clf = GridSearchCV(model, parameter, scoring='neg_log_loss', cv=nb_cross_val, n_jobs=4)
-    #     clf.fit(X, y)
-    #     res = clf.cv_results_
-    #
-    #     with open(name+'.csv', 'w') as csvfile:
-    #         writer = csv.writer(csvfile, delimiter=';')
-    #         for nb_set_params in range(len(res['params'])):
-    #             res_str = '{} {} {}'.format(res['params'][nb_set_params],
-    #                                         res['mean_test_score'][nb_set_params],
-    #                                         res['std_test_score'][nb_set_params])
-    #             writer.writerow([res_str])
-    #
-    # # for (i, name) in enumerate(names):
-    # #     print(name, results[i].keys())
+    ### ALL PARAMS
+    # Logistic Regression
+    parameters = {'penalty':['l1', 'l2'], 'C':[0.1, 1., 10.]}
+    models.append(("LogisticRegression", LogisticRegression(), parameters))
+
+    # SVM
+    parameters = {'kernel':['linear', 'rbf', 'poly', 'sigmoid'], 'C':[0.1, 1., 10., 100], 'probability':True}
+    models.append(("SVC", SVC(), parameters))
+
+    # KNeighbors
+    parameters = {'n_neighbors':[3, 5, 7], 'leaf_size':[10, 20, 30, 40], 'p':[1, 2]}
+    models.append(("KNeighbors", KNeighborsClassifier(), parameters))
+
+    # DecisionTree
+    parameters = {'splitter':['best', 'random'], 'criterion':['gini', 'entropy'],
+                  'max_depth':[3, 5, 10, 20], 'min_samples_split':[2, 3, 5]}
+    models.append(("DecisionTree", DecisionTreeClassifier(), parameters))
+
+    # RandomForestClassifier
+    parameters = {'n_estimators':[10, 50, 100], 'criterion':['gini', 'entropy'],
+                  'max_depth':[None, 3, 10], 'min_samples_split':[2, 3, 5]}
+    models.append(("RandomForest", RandomForestClassifier(), parameters))
+
+    # GradientBoostingClassifier
+    parameters = {'loss':['deviance', 'exponential'], 'n_estimators':[10, 50, 100],
+                  'criterion':['friedman_mse', 'mae'], 'max_depth':[3, 10],
+                  'min_samples_split':[2, 3, 5]}
+    models.append(("GradientBoostingClassifier", GradientBoostingClassifier(), parameters))
+
+    # Multi-Layer Classifier
+    parameters = {'hidden_layer_sizes':[(70,), (40, 40), (20, 20, 20)],
+                  'activation':['logistic', 'relu'],
+                  'batch_size':[128, 256],
+                  'alpha':[0.001, 0.0001]}
+    models.append(("MLPClassifier", MLPClassifier(), parameters))
+
+
+    results = []
+    names = []
+    nb_cross_val = 2
+    for name, model, parameter in models:
+        print(name)
+        clf = GridSearchCV(model, parameter, scoring='neg_log_loss', cv=nb_cross_val, n_jobs=4)
+        clf.fit(X, y)
+        res = clf.cv_results_
+
+        with open(name+'.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';')
+            for nb_set_params in range(len(res['params'])):
+                res_str = '{} {} {}'.format(res['params'][nb_set_params],
+                                            res['mean_test_score'][nb_set_params],
+                                            res['std_test_score'][nb_set_params])
+                writer.writerow([res_str])
+
+    # for (i, name) in enumerate(names):
+    #     print(name, results[i].keys())
---
+++ visualisation.py
@@ -0,0 +1,64 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from sklearn.model_selection import train_test_split
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from otto_challenge import load_db
+
+
+def visualisation(X, y, manifold='pca', n_components=2):
+    """
+    @brief: visualise features of data manifolded in 2 or 3 dimensions, with PCA or TSNE
+
+    @param:
+            X: ndarray, (n_samples, n_features), the data to manifold
+            y: ndarray, (n_samples,), the corresponding targets
+            manifold: 'pca' or 'tsne', choice of the algorithm to compress the features
+            n_components: dimension of the destination space, for the manifold (can be 2 or 3)
+    """
+
+    print('Training Manifold...')
+
+    if manifold == 'tsne':
+        X_manifold = TSNE(n_components=n_components, verbose=10).fit_transform(X)
+    elif manifold == 'pca':
+        X_manifold = PCA(n_components=n_components).fit_transform(X)
+
+    print('End training')
+
+    fig = plt.figure()
+
+    if n_components not in (2, 3):
+        raise ValueError('Too high dimension, no visualisation possible for now')
+    elif n_components == 3:
+        ax = fig.add_subplot(111, projection='3d')
+    elif n_components == 2:
+        ax = fig.add_subplot(111)
+
+    for class_i in np.unique(y):
+        if n_components == 2:
+            ax.scatter(X_manifold[y == class_i, 0],
+                       X_manifold[y == class_i, 1],
+                       label=str(class_i))
+        elif n_components == 3:
+            ax.scatter(X_manifold[y_train == class_i, 0],
+                       X_manifold[y_train == class_i, 1],
+                       X_manifold[y_train == class_i, 2],
+                       label=str(class_i))
+
+    plt.legend()
+    plt.show()
+
+    return X_manifold
+
+
+if __name__ == "__main__":
+
+    X, y = load_db('train.csv')
+    X_test = load_db('test.csv', converter=None, test=True)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    visualisation(X_train[:10000], y_train[:10000], manifold='tsne', n_components=2)
